04a458fef1fbddecab3b6de7aa7dde56d69e7e20
Olle Jernström <olle.jernstrom@gmail.com>
2022-03-15 12:27:29 +0100

    add icon remove cursor

---
+++ ide/src/im.rs
@@ -0,0 +1,19 @@
+use std::{
+    fs::{self, File},
+    io::Read,
+};
+
+pub fn get_logo(file_name: String) -> Vec<u8> {
+    let mut file_content = Vec::new();
+
+    let mut file = File::open(&file_name).expect("Unable to open file");
+    file.read_to_end(&mut file_content).expect("Unable to read");
+    let f2: Vec<u8> = fs::read_to_string(file_name)
+        .unwrap()
+        .split(" ")
+        .map(|num| num.parse::<u8>().unwrap())
+        .collect();
+    println!("{:?}", f2);
+    println!("{}", f2.len());
+    f2
+}
--- ide/src/main.rs
+++ ide/src/main.rs
@@ -2,32 +2,47 @@ use camera_controller::CameraController;
 use game_engine::{renderer::Instance, Context};
 
 use winit::{
+    dpi::LogicalPosition,
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
+    window::{Icon, WindowBuilder},
 };
 
+//use image;
 use env_logger;
 use vek::quaternion::repr_c::Quaternion;
 use vek::vec::repr_c::Vec3;
 
 mod camera_controller;
+mod im;
 
 const SPACE_BETWEEN: f32 = 3.0;
 const NUM_INSTANCES_PER_ROW: u32 = 4;
 
 fn update(start: std::time::Instant, objects: &mut Vec<Instance>) {
     let offset = start.elapsed().as_secs_f32().sin();
-    for obj in objects {
-
-    }
+    for obj in objects {}
 }
 
 fn main() {
     env_logger::init();
 
+    let icon_vec: Vec<u8> = vec![0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0, 0, 255, 0, 0];
+    //let icon_vec = im::get_logo("out".to_string());
+    let icon = Icon::from_rgba(icon_vec, 2, 2).unwrap();
+
     let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let window = WindowBuilder::new()
+        .with_window_icon(Some(icon))
+        .build(&event_loop)
+        .unwrap();
+
+    window.set_cursor_visible(false);
+    match window.set_cursor_grab(true) {
+        Ok(_) => (),
+        Err(e) => eprint!("{:?}", e),
+    }
+
     let size = window.inner_size();
 
     let mut context = Context::new(&window, (size.width, size.height));
@@ -35,9 +50,12 @@ fn main() {
     let model_pawn = context.renderer.load_model("./res/ball.obj").unwrap();
     let start = std::time::Instant::now();
 
-    let mut camera_controller = CameraController::new(0.2,0.01,
-        Vec3::new(-2.0, 0.0,0.0),
-        Vec3::new(-40.0f32.to_radians(), 275.0f32.to_radians(),0.0));
+    let mut camera_controller = CameraController::new(
+        0.2,
+        0.01,
+        Vec3::new(-2.0, 0.0, 0.0),
+        Vec3::new(-40.0f32.to_radians(), 275.0f32.to_radians(), 0.0),
+    );
 
     let mut instances = (0..NUM_INSTANCES_PER_ROW)
         .flat_map(|z| {
@@ -61,11 +79,15 @@ fn main() {
             })
         })
         .collect::<Vec<_>>();
-    
+
+    let mut cursor_in_window = true;
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
         match event {
-            Event::DeviceEvent { device_id: _, event } => {
+            Event::DeviceEvent {
+                device_id: _,
+                event,
+            } => {
                 camera_controller.process_device_events(&event);
             }
             Event::WindowEvent {
@@ -81,6 +103,24 @@ fn main() {
 
             Event::WindowEvent {
                 window_id: _,
+                event: WindowEvent::CursorLeft { device_id: _ },
+            } => {
+                cursor_in_window = false;
+            }
+
+            Event::WindowEvent {
+                window_id: _,
+                event: WindowEvent::CursorEntered { device_id: _ },
+            } => {
+                cursor_in_window = true;
+                match window.set_cursor_position(LogicalPosition::new(0.0, 0.0)) {
+                    Ok(_) => (),
+                    Err(e) => eprint!("{:?}", e),
+                }
+            }
+
+            Event::WindowEvent {
+                window_id: _,
                 event,
             } => {
                 camera_controller.process_window_events(&event);
@@ -88,7 +128,9 @@ fn main() {
             Event::MainEventsCleared => window.request_redraw(),
             Event::RedrawRequested(_) => {
                 update(start, &mut instances);
-                camera_controller.update_camera(&mut context.renderer.camera);
+                if cursor_in_window {
+                    camera_controller.update_camera(&mut context.renderer.camera);
+                }
                 context.renderer.update_camera();
 
                 context
