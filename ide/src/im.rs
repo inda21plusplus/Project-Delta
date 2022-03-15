8f7c20c555a1c6462348408f965e0b044109bb4e
Vincent <vincent.lagerros@gmail.com>
2022-03-15 13:58:05 +0100

    fixed icon

--- ide/src/im.rs
+++ ide/src/im.rs
@@ -3,17 +3,30 @@ use std::{
     io::Read,
 };
 
-pub fn get_logo(file_name: String) -> Vec<u8> {
-    let mut file_content = Vec::new();
+pub fn get_logo(file_name: String) -> (u32, u32, Vec<u8>) {
+    let mut bff = String::new();
 
     let mut file = File::open(&file_name).expect("Unable to open file");
-    file.read_to_end(&mut file_content).expect("Unable to read");
-    let f2: Vec<u8> = fs::read_to_string(file_name)
-        .unwrap()
-        .split(" ")
-        .map(|num| num.parse::<u8>().unwrap())
-        .collect();
-    println!("{:?}", f2);
-    println!("{}", f2.len());
-    f2
+    file.read_to_string(&mut bff).expect("Unable to read");
+    let mut lines = bff.lines();
+    lines.next();
+    let (width_str, height_str) = lines.next().unwrap().split_once(' ').unwrap();
+    let width: usize = width_str.parse().unwrap();
+    let height: usize = height_str.parse().unwrap();
+    let mut content: Vec<u8> = Vec::with_capacity(width * height * 4);
+    for _ in 0..height {
+        for _ in 0..width {
+            let mut line = lines.next().unwrap().splitn(3, ' ');
+            let r: u8 = line.next().unwrap().parse().unwrap();
+            let g: u8 = line.next().unwrap().parse().unwrap();
+            let b: u8 = line.next().unwrap().parse().unwrap();
+            content.push(b);
+            content.push(r);
+            content.push(g);
+
+            content.push(255);
+        }
+    }
+
+    (width as u32, height as u32, content)
 }
